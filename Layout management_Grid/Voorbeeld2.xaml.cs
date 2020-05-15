f55a58795966251aabc1721697072f9799eab20f
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-05-15 20:17:33 +0200

    layout management_grid

---
+++ Layout management_Grid/Voorbeeld2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Layout_management_Grid
+{
+    /// <summary>
+    /// Interaction logic for Voorbeeld2.xaml
+    /// </summary>
+    public partial class Voorbeeld2 : Window
+    {
+        public Voorbeeld2()
+        {
+            InitializeComponent();
+        }
+    }
+}
